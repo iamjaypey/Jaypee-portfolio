44b43aed1601466445085279c2d817cc55467744
iamjaypey <iamjaypey1@gmail.com>
2022-09-07 08:43:16 +0100

    in creating the testimonial section, i added pagination to the swiper functions

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,16 +1,66 @@
 import React from 'react';
 import './Testimonials.css';
-
-
-
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import profilePic1 from '../../img/profile1.jpg';
+import profilePic2 from '../../img/profile2.jpg';
+import profilePic3 from '../../img/profile3.jpg';
+import profilePic4 from '../../img/profile4.jpg';
+import {Pagination} from 'swiper'
+import 'swiper/css/pagination';
+import 'swiper/css';
 
 const Testimonials = () => {
+
+   const clients = [
+        {
+        img: profilePic1,
+        review:
+            "lorem Ipsum is just one character in the first world. Lorem Ipsum is just one character in the first world"
+        },{
+            img: profilePic2,
+        review:
+            "lorem Ipsum is just one character in the first world. Lorem Ipsum is just one character in the first world"
+        },
+        {
+            img: profilePic3,
+        review:
+            "lorem Ipsum is just one character in the first world. Lorem Ipsum is just one character in the first world"
+        },
+        {
+            img: profilePic4,
+        review:
+            "lorem Ipsum is just one character in the first world. Lorem Ipsum is just one character in the first world"
+        }
+    ]
+
   return (
-    <div className="t-wwrapper">
+    <div className="t-wrapper">
         <div className="t-heading">
-            
+            <span>Clients Reviews </span>
+            <span>from my</span>
+            <span> Exceptional work</span>
+            <div className="blur t-blur1" style={{background: "var(--purple)"}}></div>
+            <div className="blur t-blur2" style={{background: "skyblue"}}></div>
         </div>
+
+        {/* slider */}
+        <Swiper
+        modules={[Pagination]}
+        slidesPerView={1}
+        Pagination={{clickable: true}}
+        >
+            {clients.map((client, index) => {
+            return (
+                <SwiperSlide key={index}>
+                    <div className="testimonial"></div>
+                    <img src={client.img} alt="" />
+                    <span>{client.review} </span>
+                </SwiperSlide>
+            )
+})}
+
+
+        </Swiper>
     </div>
   )
 }
